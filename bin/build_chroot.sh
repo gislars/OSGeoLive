b3c0bf50d20452a585bdc2b1c46a54ce23ab8158
gislars <lars@lingner.eu>
2025-03-16 22:41:04 +0100

    Use UDF format to be not limitted to 4GB

--- bin/build_chroot.sh
+++ bin/build_chroot.sh
@@ -412,6 +412,7 @@ if [ "$ARCH" = "amd64" ] ; then
       -eltorito-alt-boot \
       -e '--interval:appended_partition_2:::' \
       -no-emul-boot \
+      -udf \
       -o ../"$ISO_NAME.iso" .
    # sudo isohybrid -u ../"$ISO_NAME.iso"
 else
